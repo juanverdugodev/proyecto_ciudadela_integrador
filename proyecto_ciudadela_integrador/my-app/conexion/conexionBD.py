51cde8ef309c6cf64185d8cbfe302cc7ea5004c3
juanverdugodev <juanf.verdugo@ucuenca.edu.ec>
2025-01-21 15:59:09 -0500

    Ajuste de Conexion_BD

--- proyecto_ciudadela_integrador/my-app/conexion/conexionBD.py
+++ proyecto_ciudadela_integrador/my-app/conexion/conexionBD.py
@@ -1,30 +1,21 @@
-
-
-# Importando Libreria mysql.connector para conectar Python con MySQL
 import mysql.connector
-
+from mysql.connector import Error
 
 def connectionBD():
-    print("ENTRO A LA CONEXION")
+    print("Conectando a la base de datos...")
     try:
-        # connection = mysql.connector.connect(
         connection = mysql.connector.connect(
             host="34.68.99.255",
-                #host="viaduct.proxy.rlwy.net",
             port=3306,
             user="Administrador",
             passwd="PI_ciudadela",
-                #passwd="-D2eD6aDb5Bg6dEbhAAeBB6gd3EheaBg",
             database="ciudadela",
-                #database="crud_python",
-            charset='utf8mb4',
-            collation='utf8mb4_unicode_ci',
             raise_on_warnings=True
-
         )
+        
         if connection.is_connected():
-            print("Conexión exitosa a la BD")
+            print("Conexión exitosa a la base de datos.")
             return connection
 
     except mysql.connector.Error as error:
-        print(f"No se pudo conectar: {error}")
+        print(f"No se pudo conectar a la base de datos.\nError encontrado: {error}\n")
