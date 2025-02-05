a1c170c4ad40c5ce34d169e5181f5de4af61bff4
juanverdugodev <juanf.verdugo@ucuenca.edu.ec>
2025-02-05 04:41:34 -0500

    Cambios interfaz Juan 🐎

--- proyecto_ciudadela_integrador/my-app/conexion/conexionBD.py
+++ proyecto_ciudadela_integrador/my-app/conexion/conexionBD.py
@@ -1,21 +1,27 @@
 import mysql.connector
 from mysql.connector import Error
 
-def connectionBD():
-    print("Conectando a la base de datos...")
-    try:
-        connection = mysql.connector.connect(
-            host="127.0.0.1",
-            port=3306,
-            user="root",
-            passwd="1234",
-            database="ciudadela",
-            raise_on_warnings=True
-        )
-        
-        if connection.is_connected():
-            print("Conexión exitosa a la base de datos.")
-            return connection
+# Variable global para mantener la conexión a la base de datos
+connection = None
 
-    except mysql.connector.Error as error:
-        print(f"No se pudo conectar a la base de datos.\nError encontrado: {error}\n")
+def connectionBD():
+    global connection
+    if connection is None or not connection.is_connected():
+        print("Conectando a la base de datos...")
+        try:
+            connection = mysql.connector.connect(
+                host="34.68.99.255",
+                port=3306,
+                user="Administrador",
+                passwd="@PI_2ciudadela",
+                database="ciudadela",
+                raise_on_warnings=True
+            )
+            if connection.is_connected():
+                print("Conexión exitosa a la base de datos.")
+        except mysql.connector.Error as error:
+            print(f"No se pudo conectar a la base de datos.\nError encontrado: {error}\n")
+    return connection
+
+# Uso de la conexión de base de datos
+db_connection = connectionBD()
